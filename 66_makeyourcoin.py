1ab8074bfcf78f79e8d63f34351a3aa3696e885a
Marcia Tobias <marciaatobiass@gmail.com>
2018-08-27 13:19:14 +0100

    creating a template to make a coin of pound

---
+++ 66_makeyourcoin.py
@@ -0,0 +1,21 @@
+class Pound:
+
+    value = 1.00
+    colour = "gold"
+    num_edges = 1
+    diameter = 22.5 #mm
+    thickness = 3.15 #mm
+    heads = True
+
+
+coin1 = Pound()
+
+
+coin1.colour = "greenish"
+
+coin2 = Pound()
+
+print(coin1.colour)
+print(coin2.colour)
+
+
