f068f76dc2f36e1d3b3938b08ec89575c82399ce
Marcia Tobias <marciaatobiass@gmail.com>
2018-08-27 16:12:43 +0100

    creating class method del, rust, clean, flip and import(random)

--- 66_makeyourcoin.py
+++ 66_makeyourcoin.py
@@ -1,21 +1,39 @@
+import(random)
+
 class Pound:
 
-    value = 1.00
-    colour = "gold"
-    num_edges = 1
-    diameter = 22.5 #mm
-    thickness = 3.15 #mm
-    heads = True
+    def __init__(self, rare = False): #init is constructor
+
+        self.rare = rare
+
+        if self.rare
+            self.value = 1.25
+        else:
+            self.value = 1.00
 
 
-coin1 = Pound()
+        self.value = 1.00
+        self.colour = "gold"
+        self.num_edges = 1
+        self.diameter = 22.5 #mm
+        self.thickness = 3.15 #mm
+        self.heads = True
+
+    def __del__(self): #del is destructor
+        print("Coin Spent")
+
+    def rust(self):
+        self.colour = "greenish"
+
+    def clean(self):
+        self.colour = "gold"
+
+    def flip(self):
+        heads_options = [True, False]
+        choice = random.choice(heads_options)
+        self.heads = choice
 
 
-coin1.colour = "greenish"
 
-coin2 = Pound()
-
-print(coin1.colour)
-print(coin2.colour)
 
 
