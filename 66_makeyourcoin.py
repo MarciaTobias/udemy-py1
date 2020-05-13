c50729326ecdf9c63742201027ea8e4dbfda9b8e
Marcia Tobias <marciaatobiass@gmail.com>
2020-05-13 18:41:11 +0100

    Update 66_makeyourcoin.py
    Included Parent class and child pound

--- 66_makeyourcoin.py
+++ 66_makeyourcoin.py
@@ -1,37 +1,65 @@
-import(random)
+import random
 
-class Pound:
+#Parent class
+class Coin:
 
-    def __init__(self, rare = False): #init is constructor
+    #Constructor
+    def __init__(self, rare = False, clean = True, heads = True, **kwargs): #properties, **kwargs pack the data down in dictionary called kwargs
+        
+        # Loop trough the kwags dictionary
+        for key,value in kwargs.items():
+            settattr(self,key,value)
 
-        self.rare = rare
 
-        if self.rare
-            self.value = 1.25
+        self.is_rare = rare
+        self.is_clean = clean
+        self.heads = heads
+
+        if self.is_rare:
+            self.value = self.original_value * 1.25
         else:
-            self.value = 1.00
+            self.value = self.original_value
 
+        if self.is_clean:
+            self.colour = self.clean_colour
+        else:
+            self.colour = self.rusty_colour
 
-        self.value = 1.00
-        self.colour = "gold"
-        self.num_edges = 1
-        self.diameter = 22.5 #mm
-        self.thickness = 3.15 #mm
-        self.heads = True
+    def rust(self):
+        self.colour = self.rusty_colour
+
+    def clean(self):
+        self.colour = self.clean_colour
 
     def __del__(self): #del is destructor
         print("Coin Spent")
-
-    def rust(self):
-        self.colour = "greenish"
-
-    def clean(self):
-        self.colour = "gold"
 
     def flip(self):
         heads_options = [True, False]
         choice = random.choice(heads_options)
         self.heads = choice
+
+#Child class (inheritance)
+class Pound(Coin):
+
+    def __init__(self):
+        # Dictionary for the data pound class. This is specific for each coin
+        data = {
+            "original_value": 1.00,
+            "clean_colour": "gold",
+            "rusty_colour": "greenish",
+            "num_edges": 1,
+            "diameter": 22.5,
+            "thickness": 3.15,
+            "mass": 9.5
+        }
+        # Super Class means parent class. It will run the constructor and pass all the data as keyword arguments. **data is used to unpack the data
+        super().__init__(**data)
+
+
+   
+
+
 
 
 
